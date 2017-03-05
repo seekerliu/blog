43cc6290c2c33cd85ac284eba2bc0dbe6fd5dd5e
seekerliu <seekerliu@vip.qq.com>
2017-03-05 16:12:33 +0800

    change app name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'SeekerLiu & Leijingwei \'s Blog',
+    'name' => 'SeekerLiu & Leijingwei \'s',
 
     /*
     |--------------------------------------------------------------------------
